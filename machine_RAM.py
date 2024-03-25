afc595d3d025caab0d408633461dd0dba2f055ec
raphaelDupuy <raphael.dupuy@ens.uvsq.fr>
2024-03-25 20:31:02 +0100

    ajout pattern matching instructions

--- machine_RAM.py
+++ machine_RAM.py
@@ -5,6 +5,7 @@ class machine(object):
         self.prog = programme
         self.etape = 0
         self.entree = entree
+        self.travail = [0]
         self.sortie = []
 
     def get_etape(self) -> int:
@@ -27,16 +28,37 @@ class machine(object):
         
     def set_etape(self, nouvelle_etape):
         self.etape = nouvelle_etape
+
+    def valeur(self, val: str) -> int:
+        if (registre := val[0]) == "R":
+            return self.travail[int(val.split("R")[1])]
+        elif registre == "I":
+            return self.entree[int(val.split("I")[1])]
+        elif registre == "O":
+            return self.sortie[int(val.split("O")[1])]
+        else:
+            return int(val)
         
     def next(self):
-        print(self.get_instr().split("(")[1].split(")")[0])
-        match self.get_instr().split("(")[1].split(")")[0]:
+        match self.get_instr().split("(")[0], self.get_instr().split("(")[1].split(")")[0].split(","):
+
             case["JE", args]:
-                print("JE" + args)
+                if self.valeur(args[0]) == self.valeur(args[1]):
+                    self.set_etape(self.get_etape() + int(args[2]))
+
             case["ADD", args]:
-                print("ADD" + args)
+                print("ADD ")
+                if (registre := args[2]) == "R":
+                    self.travail[int(args[2].split("R")[1])] = self.valeur(args[0]) + self.valeur(args[1])
+                elif registre == "I":
+                    self.entree[int(args[2].split("I")[1])] = self.valeur(args[0]) + self.valeur(args[1])
+                elif registre == "O":
+                    self.sortie[int(args[2].split("O")[1])] = self.valeur(args[0]) + self.valeur(args[1])
+                    self.set_etape(self.get_etape() + 1)            
+
             case["JUMP", args]:
-                print("JUMP" + args)
+                    self.set_etape(self.get_etape() + int(args[2]))
+
 
 
     def __str__(self) -> str:
